2b0169a8e466e2d32cc8e0d0e15d81e8b1b5f72b
mansi tomar <mansitomar9582@gmail.com>
2026-08-22 18:27:43 +0530

    add new file

--- app.py
+++ app.py
@@ -14,6 +14,13 @@ def home():
 
 @app.route('/predict_api', methods=['POST'])
 def predict_api():
+#     data=request.json['data']
+#     print(data)
+#     print(np.array(list(data.values())).reshape(1,-1))
+#     new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
+#     output=regmodel.predict(new_data)
+#     print(output)
+#     return jsonify(output[0])
     data = request.json['data']
 
     features = [
@@ -35,6 +42,15 @@ def predict_api():
     print("Prediction:", output)
 
     return jsonify(float(output[0]))
+
+# now we have to create a html page where user enter the values and predict the price 
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.value()]
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    regmodel.predict(final_input)[0]
+    return render_template("home.html",prediction_text="the predicted price is :" .format(output))
 
 if __name__=="__main__":
     app.run(debug=False)
